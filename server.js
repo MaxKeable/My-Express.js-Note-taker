563736355a2f2e22254a5a24cc772a32e7ee5e4a
MaxKeable <max.keable488@gmail.com>
2023-04-29 10:19:57 +1000

    Fixing file structure -moving public folder outside of develop and moving package.json

---
+++ server.js
@@ -0,0 +1,21 @@
+// Imports and setting consts
+const express = require('express');
+const html_routes = require('./routes/html-routes')
+const api_routes = require('./routes/api-routes')
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware to run operations top to bottom
+app.use(express.urlencoded({ 
+    extended: false 
+})
+);
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(html_routes)
+app.use(api_routes)
+
+app.listen(PORT, () => {
+    console.log(`The server is running on http://localhost:${PORT}`);
+});
