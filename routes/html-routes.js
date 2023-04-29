2b9da3a77d9483b13905eca63ef92ca61fa62ce8
MaxKeable <max.keable488@gmail.com>
2023-04-29 10:03:39 +1000

    adding logic to html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const path = require('path');
+
+// Sets up the route that sends the index.html to the client
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+// sets up the route that sends notes.html to the client
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+module.exports = router;
